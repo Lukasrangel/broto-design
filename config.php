c6c8ca6da240e513a6dd2e6a7556cf233cd5112e
Lukas Dev <103273579+Lukasrangel@users.noreply.github.com>
2024-07-23 19:41:10 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,25 +1,11 @@
 <?php
 session_start();
 
-
 date_default_timezone_set('America/Sao_Paulo');
-
-$autoload = function($class){
-    if($class == "Email"){
-        include('classes/PHPMailer/src/Exception.php');
-        include('classes/PHPMailer/src/PHPMailer.php');
-        include('classes/PHPMailer/src/SMTP.php');
-    }
-    include('classes/' . $class . ".php");
-
-};
-
-spl_autoload_register($autoload);
 
 
 //Indexes
-define('INITIAL_PATH', 'https://luksdev.cloud/broto/');
-
+define('INITIAL_PATH', 'http://127.0.0.1/broto/');
 
 
 ?>
